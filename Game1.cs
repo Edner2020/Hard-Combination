9a91e208604cad663686ee12cc3a8609c5600cbd
Edner2020 <toha.dimas@gmail.com>
2016-03-24 18:27:00 +0400

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media; 
+
 
 
 namespace Game2
@@ -21,17 +22,24 @@ namespace Game2
         Texture2D hero;
         Texture2D blok_1, blok_2;
         Texture2D floor;
-        Rectangle hero_pos = new Rectangle();
-        Vector2 blok_1_pose = new Vector2();
-        Vector2 blok_2_pose = new Vector2();
+        Rectangle hero_pos = new Rectangle(0,450,30,30);
+        Vector2 blok_1_pose = new Vector2(180,450);
+        Vector2 blok_2_pose = new Vector2(210,420);
         float timeForJump = 0.3f;
         float time_jump = 0;
+        bool jump = false;
+        List<Box> boxs;
+        
  
       
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+           graphics.PreferredBackBufferHeight =  480;
+
+            graphics.PreferredBackBufferWidth = 600; 
+            
         }
 
       
@@ -41,48 +49,16 @@ namespace Game2
 
             base.Initialize();
         }
-
-        void Greatemap()
-        {
-            int[,] map;
-
-            
-            
-   map = new int[,] 
-  {{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,0,0,2}, 
-  {2,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2}, 
-  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,2}, 
-  {2,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,2}, 
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
-   int x = 0;
-   int y = 0;
-   for (int i = 0; i < map.GetLength(1); i++)
-   {
-       for (int j = 0; j < map.GetLength(2); j++)
-       {
-           Rectangle rect = new Rectangle(x,y,30,30);
-           int a = map[i, j];
-
-
-
-       }
-
-   }
-
-
-            
-
-        }
+        
+      
         protected override void LoadContent()
         {
-            
+            Greatemap();
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            hero = Content.Load<Texture2D>("hero.tif");
+            blok_1= Content.Load<Texture2D>("blok_1.jpg");
+           blok_2 = Content.Load<Texture2D>("blok_2.png");
+           Greatemap();
 
         }
 
@@ -92,24 +68,83 @@ namespace Game2
             
         }
 
+
+        void Greatemap()
+        {
+            int[,] map;
+
+            boxs = new List<Box>();
+
+            map = new int[16,20] 
+  {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Rectangle rect = new Rectangle(x, y, 30, 30);
+                    int a = map[i, j];
+
+                    if (a == 1)
+                    {
+                        Box box = new Box(blok_1,rect);
+                        boxs.Add(box);
+                       
+
+                    }
+                 
+                    x += 30;
+                }
+                x = 0;
+                y += 30;
+            }
+
+        }
        
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                hero_pos.X += 2;
 
-           
+                
+            }
 
+            Greatemap();
             base.Update(gameTime);
         }
 
         
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.LightGray);
+            GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
-
+            foreach (Box box in boxs)
+            {
+                box.Draw(spriteBatch);
+            } 
+            spriteBatch.Draw(hero, hero_pos, Color.White);
+            spriteBatch.Draw(blok_1, blok_1_pose, Color.White);
+            spriteBatch.Draw(blok_2, blok_2_pose, Color.White);
             spriteBatch.End();
           
 
